067b3f9de266f198471a56b2c006897d0151922e
Rna341 <rna.tariq.aboouf@gmail.com>
2025-01-01 17:08:33 +0200

    Fixed post user details retrieving

--- front-end/src/Post/Post.js
+++ front-end/src/Post/Post.js
@@ -15,37 +15,32 @@ const Post = ({ post, jwt, handleEditPost, handleDeletePost, handleLike }) => {
         <div className="post">
             <div className="post-header">
                 <div className="post-user-info">
-                    {post.user_id?.avatar ? (
-                        <img
-                            src={`http://localhost:3000/uploads/${post.user_id.avatar}`}
-                            alt={`${post.user_id?.firstName || 'User'}'s avatar`}
-                            className="user-avatar"
-                        />
-                    ) : (
-                        <div className="default-avatar">U</div> 
-                    )}
+                    <div className="default-avatar">
+                        {post.user_id?.firstName?.[0] || 'U'}
+                    </div>
                     <div className="post-user-details">
                         <span className="user-name">
-                            {post.user_id?.firstName || 'Unknown'} {post.user_id?.lastName || ''}
+                            {post.user_id?.firstName} {post.user_id?.lastName}
                         </span>
                         <span className="post-date">{formattedDate(post.created_at)}</span>
                     </div>
                 </div>
-
-                <div className="post-dropdown">
-                    <button className="dropdown-toggle">...</button>
-                    <div className="dropdown-menu">
-                        <button
-                            onClick={() => {
-                                setEditingPost(post._id);
-                                setEditingContent(post.content);
-                            }}
-                        >
-                            Edit
-                        </button>
-                        <button onClick={() => handleDeletePost(post._id)}>Delete</button>
+                {post.user_id?._id === jwt && (
+                    <div className="post-dropdown">
+                        <button className="dropdown-toggle">...</button>
+                        <div className="dropdown-menu">
+                            <button
+                                onClick={() => {
+                                    setEditingPost(post._id);
+                                    setEditingContent(post.content);
+                                }}
+                            >
+                                Edit
+                            </button>
+                            <button onClick={() => handleDeletePost(post._id)}>Delete</button>
+                        </div>
                     </div>
-                </div>
+                )}
             </div>
 
             {editingPost === post._id ? (
@@ -56,17 +51,23 @@ const Post = ({ post, jwt, handleEditPost, handleDeletePost, handleLike }) => {
                         rows="3"
                         placeholder="Edit your post"
                     />
-                    <button onClick={() => {
-                        handleEditPost(post._id, editingContent);
-                        setEditingPost(null);
-                    }}>Save</button>
-                    <button onClick={() => setEditingPost(null)}>Cancel</button>
+                    <div className="edit-buttons">
+                        <button
+                            onClick={() => {
+                                handleEditPost(post._id, editingContent);
+                                setEditingPost(null);
+                            }}
+                        >
+                            Save
+                        </button>
+                        <button onClick={() => setEditingPost(null)}>Cancel</button>
+                    </div>
                 </div>
             ) : (
                 <>
                     {post.photo && (
                         <img
-                            src={`http://localhost:3000/uploads/${post.photo}`}
+                            src={`/uploads/${post.photo}`}
                             alt="Post"
                             className="post-photo"
                         />
@@ -74,16 +75,15 @@ const Post = ({ post, jwt, handleEditPost, handleDeletePost, handleLike }) => {
                     <p className="post-content">{post.content}</p>
                     <div className="post-actions">
                         <button
-                            onClick={() => handleLike(post._id, post.likes.includes(jwt))}
-                            className={`like-button ${post.likes.includes(jwt) ? 'liked' : ''}`}
+                            onClick={() => handleLike(post._id)}
+                            className={`like-button ${post.likes?.includes(jwt) ? 'liked' : ''}`}
                         >
-                            {post.likes.includes(jwt) ? 'Unlike' : 'Like'}
+                            Like
                         </button>
+                        <span>{post.num_like} Likes</span>
                     </div>
-                    <span>{post.num_like} Likes</span>
                 </>
             )}
-
             <Comment
                 comments={post.comments}
                 postId={post._id}

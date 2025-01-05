bcca1498bbda1f00b9fd28fdf3aa18708ee48b5b
Rna341 <rna.tariq.aboouf@gmail.com>
2025-01-05 17:03:15 +0200

    Handled routing

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from '../src/App/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+
+const root = createRoot(container);
+
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  );
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);

f8c4981f95c421e8c7d01ba62dc32e59bce3353d
Rna341 <rna.tariq.aboouf@gmail.com>
2025-01-12 16:25:13 +0200

    Added post context to share same posts state

--- front-end/src/App/App.js
+++ front-end/src/App/App.js
@@ -6,65 +6,68 @@ import Feed from '../Feed/Feed';
 import Profile from '../Profile/Profile';
 import Header from '../Header/Header';
 import ScrollToTopButton from '../ScrollToTopButton/ScrollToTopButton';
+import { PostsProvider } from '../Post/PostsContext';
 
 const App = () => {
-  const [jwt, setJwt] = useState(null);
-  const [isLogin, setIsLogin] = useState(true);
-  const location = useLocation();
+    const [jwt, setJwt] = useState(null);
+    const [isLogin, setIsLogin] = useState(true);
+    const location = useLocation();
 
-  const handleLogin = (token) => {
-    setJwt(token);
-  };
+    const handleLogin = (token) => {
+        setJwt(token);
+    };
 
-  const toggleForm = () => {
-    setIsLogin(!isLogin);
-  };
+    const toggleForm = () => {
+        setIsLogin(!isLogin);
+    };
 
-  const handleRegisterSuccess = () => {
-    setIsLogin(true);
-  };
+    const handleRegisterSuccess = () => {
+        setIsLogin(true);
+    };
 
-  const isFeedOrProfile = location.pathname.startsWith('/feed') || location.pathname.startsWith('/profile');
+    const isFeedOrProfile = location.pathname.startsWith('/feed') || location.pathname.startsWith('/profile');
 
-  return (
-    <div className="app-container">
-      {jwt && <Header jwt={jwt} setJwt={setJwt} />}
-      <Routes>
-        {jwt ? (
-          <>
-            <Route path="/feed" element={<Feed jwt={jwt} />} />
-            <Route path="/profile/:userId" element={<Profile jwt={jwt} />} />
-            <Route path="*" element={<Navigate to="/feed" />} />
-          </>
-        ) : (
-          <>
-            <Route
-              path="/login"
-              element={
-                isLogin ? (
-                  <>
-                    <Login onLogin={handleLogin} />
-                    <button className="toggle-button" onClick={toggleForm}>
-                      Don't have an account? Sign up
-                    </button>
-                  </>
-                ) : (
-                  <>
-                    <Register onRegisterSuccess={handleRegisterSuccess} />
-                    <button className="toggle-button" onClick={toggleForm}>
-                      Already have an account? Sign in
-                    </button>
-                  </>
-                )
-              }
-            />
-            <Route path="*" element={<Navigate to="/login" />} />
-          </>
-        )}
-      </Routes>
-      {isFeedOrProfile && <ScrollToTopButton />}
-    </div>
-  );
+    return (
+        <div className="app-container">
+            <PostsProvider>
+                {jwt && <Header jwt={jwt} setJwt={setJwt} />}
+                <Routes>
+                    {jwt ? (
+                        <>
+                            <Route path="/feed" element={<Feed jwt={jwt} />} />
+                            <Route path="/profile/:userId" element={<Profile jwt={jwt} />} />
+                            <Route path="*" element={<Navigate to="/feed" />} />
+                        </>
+                    ) : (
+                        <>
+                            <Route
+                                path="/login"
+                                element={
+                                    isLogin ? (
+                                        <>
+                                            <Login onLogin={handleLogin} />
+                                            <button className="toggle-button" onClick={toggleForm}>
+                                                Don't have an account? Sign up
+                                            </button>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <Register onRegisterSuccess={handleRegisterSuccess} />
+                                            <button className="toggle-button" onClick={toggleForm}>
+                                                Already have an account? Sign in
+                                            </button>
+                                        </>
+                                    )
+                                }
+                            />
+                            <Route path="*" element={<Navigate to="/login" />} />
+                        </>
+                    )}
+                </Routes>
+                {isFeedOrProfile && <ScrollToTopButton />}
+            </PostsProvider>
+        </div>
+    );
 };
 
 export default App;
